d0ef1724f8baea4c6749c7bc652ca6f5eec610a0
lolllSkYGEN <ppwhitee@mail.ru>
2025-11-28 21:11:59 +0300

    Update server.py

--- server.py
+++ server.py
@@ -87,4 +87,6 @@ def list_keys():
     return jsonify(load_keys())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 10000))  # ← Render ожидает 10000 по умолчанию
+    app.run(host="0.0.0.0", port=port)
+
